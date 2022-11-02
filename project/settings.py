ddbdf6657465ae532f16fd5baea24a49d2ed7aea
Behzod <behzodtuit@gmail.com>
2022-11-02 21:10:23 +0500

    finish

--- project/settings.py
+++ project/settings.py
@@ -51,6 +51,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8080',
     'http://localhost:8080',
     'https://avicennafront.netlify.app',
+    'https://davomat.avitsennaqarshi.uz'
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
